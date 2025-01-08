a7899cf53e281ecd0494ffcc6739b313b7fee6df
rakibul islam <rakibulislam325558@gmail.com>
2025-01-08 23:33:24 +0600

    Quary selector

---
+++ Quary Selector/Myscript.js
@@ -0,0 +1,7 @@
+function ChangeFu(){
+    document.querySelector('h1').innerHTML="Rakibul islam "
+    document.querySelector('#ddd').innerHTML="H2 Manupuleted"
+    document.querySelector('.Myp').innerHTML="Change my peragraph"
+    document.querySelector('h4[name="Myh4"]').innerHTML="H4 Manupulated"
+
+}
